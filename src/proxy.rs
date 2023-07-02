bdb1fe6dca7a6da79c918303cae6cdc73f3f0c88
Libaration <libaration@gmail.com>
2023-07-02 16:39:59 -0400

    lets never speak of this broken implementation again

--- src/proxy.rs
+++ src/proxy.rs
@@ -1,4 +1,4 @@
-use std::io::Read;
+use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 
@@ -13,6 +13,7 @@ pub struct Client {
     pub host: String,
     pub port: u16,
     pub connection_state: ConnectionState,
+    pub server: Option<Server>,
 }
 
 impl Client {
@@ -21,6 +22,7 @@ impl Client {
             host: String::from("127.0.0.1"),
             port: 38101,
             connection_state: ConnectionState::Disconnected,
+            server: None,
         }
     }
 
@@ -29,7 +31,9 @@ impl Client {
         let listener = TcpListener::bind((self.host.as_str(), self.port)).expect("Failed to bind");
 
         println!("Listening on {}:{}", self.host, self.port);
-
+        let server = Server::new(String::from("54.165.147.223"), 38101);
+        self.server = Some(server);
+        self.server.as_ref().unwrap().connect();
         match listener.accept() {
             Ok((mut stream, addr)) => {
                 println!("New connection: {}", addr);
@@ -61,18 +65,45 @@ impl Client {
     }
 }
 
+#[derive(Debug)]
 pub struct Server {
-    pub host: Option<String>,
-    pub port: Option<u16>,
+    pub host: String,
+    pub port: u16,
     pub connection_state: ConnectionState,
 }
 
 impl Server {
-    pub fn new() -> Server {
+    pub fn new(host: String, port: u16) -> Server {
         Server {
-            host: None,
-            port: None,
+            host,
+            port,
             connection_state: ConnectionState::Disconnected,
+        }
+    }
+    pub fn connect(&self) {
+        println!("Connecting to server...");
+        let mut stream = TcpStream::connect(
+            self.host.as_str().to_owned() + ":" + self.port.to_string().as_str(),
+        )
+        .expect("Failed to connect");
+        println!("Connected to server");
+        let mut buffer = [0; 1024];
+        loop {
+            match stream.read(&mut buffer) {
+                Ok(n) => {
+                    if n == 0 {
+                        println!("Connection closed by remote end");
+                        break;
+                    }
+                    let message = String::from_utf8_lossy(&buffer[..n]);
+                    println!("Received message: {}", message);
+                    println!("{:?}", &buffer)
+                }
+                Err(e) => {
+                    println!("Error reading from socket: {}", e);
+                    break;
+                }
+            }
         }
     }
 }
