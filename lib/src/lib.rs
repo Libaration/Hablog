9d36562d3b58f667691c84f60a6dd9e195d2ba34
Libaration <libaration@gmail.com>
2023-07-04 01:03:49 -0400

    BuffWriter is "supposed" to be faster. Also error add error handling for mistakes during debugging.

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,4 +1,14 @@
+use std::io::{self, BufWriter, Write};
+
 #[no_mangle]
 pub fn parse(data: &mut [u8], origin: &mut String) {
-    println!("{} : {}", origin, hex::encode(data));
+    let encoded_data = hex::encode(data);
+    let output = format!("{} : {}\n", origin, encoded_data);
+
+    let stdout = io::stdout();
+    let mut handle = BufWriter::new(stdout.lock());
+
+    if let Err(_) = handle.write_all(output.as_bytes()) {
+        println!("error");
+    }
 }
