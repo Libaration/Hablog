0c63066ff96ea7b5dd627908f5fad64daf32e412
Libaration <libaration@gmail.com>
2023-07-12 14:57:52 -0400

    :recycle: Refactor code for proxy server to improve performance and readability

--- src/proxy.rs
+++ src/proxy.rs
@@ -2,13 +2,11 @@ use crate::{
     connection::{Connection, ConnectionState},
     hosts,
     logger::ConsoleLogger,
+    packet_handler::packet_handler::PacketHandler,
 };
 use std::sync::Arc;
-use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
-    sync::Mutex,
-};
-
+use tokio::io::{AsyncReadExt, BufReader};
+use tokio::sync::Mutex;
 #[derive(Debug)]
 pub struct Proxy<'a> {
     pub connection: &'a mut Connection,
@@ -18,6 +16,7 @@ impl Proxy<'_> {
     pub fn new(connection: &mut Connection) -> Proxy<'_> {
         Proxy { connection }
     }
+
     pub async fn start_proxy(&mut self) {
         ConsoleLogger::info(format!(
             "Starting proxy on {}:{}",
@@ -34,24 +33,21 @@ impl Proxy<'_> {
             .await
             .unwrap()
             .into_split();
-        let client_arc_read = Arc::new(Mutex::new(client_socket.0));
-        let server_arc_read = Arc::new(Mutex::new(server_socket.0));
-        let client_arc_write = Arc::new(Mutex::new(client_socket.1));
-        let server_arc_write = Arc::new(Mutex::new(server_socket.1));
-        let forward_buffers_client_to_server = Self::forward_buffers(
-            client_arc_read.clone(),
-            server_arc_write.clone(),
-            "Out".to_string(),
-        );
-        let forward_buffers_server_to_client = Self::forward_buffers(
-            server_arc_read.clone(),
-            client_arc_write.clone(),
-            "In".to_string(),
-        );
-        tokio::join!(
+
+        let forward_buffers_client_to_server = tokio::spawn(async move {
+            Self::forward_buffers(client_socket.0, server_socket.1, "Out".to_string()).await;
+        });
+
+        let forward_buffers_server_to_client = tokio::spawn(async move {
+            Self::forward_buffers(server_socket.0, client_socket.1, "In".to_string()).await;
+        });
+
+        let (res1, res2) = tokio::join!(
             forward_buffers_client_to_server,
             forward_buffers_server_to_client
         );
+        res1.unwrap();
+        res2.unwrap();
     }
 
     pub async fn wait_for_listener_connection(
@@ -63,6 +59,7 @@ impl Proxy<'_> {
             "Waiting for client to connect on {}:{}",
             self.connection.client_host, self.connection.port
         ));
+
         let listener = tokio::net::TcpListener::bind(format!(
             "{}:{}",
             self.connection.client_host, self.connection.port
@@ -70,20 +67,18 @@ impl Proxy<'_> {
         .await
         .unwrap();
         let connection = self.connection.clone();
-        tokio::spawn(async move {
-            let (client_stream, client_address) =
-                if let Ok((stream, address)) = listener.accept().await {
-                    (stream, address)
-                } else {
-                    ConsoleLogger::error("Failed to accept client connection");
-                    hosts::remove_proxy_if_exists(connection.game_host.as_str()).await;
-                    std::process::exit(1);
-                };
-            ConsoleLogger::success(format!("Caught client connection from {}", client_address));
-            client_stream.set_nodelay(true).unwrap();
-            return client_stream;
-        })
-        .await
+
+        let (client_stream, client_address) = if let Ok((stream, address)) = listener.accept().await
+        {
+            (stream, address)
+        } else {
+            ConsoleLogger::error("Failed to accept client connection");
+            hosts::remove_proxy_if_exists(connection.game_host.as_str()).await;
+            std::process::exit(1);
+        };
+        ConsoleLogger::success(format!("Caught client connection from {}", client_address));
+        client_stream.set_nodelay(true).unwrap();
+        return Ok(client_stream);
     }
 
     pub async fn wait_for_server_connection(
@@ -94,44 +89,37 @@ impl Proxy<'_> {
             self.connection.game_resolved_ip.unwrap(),
             self.connection.port
         ));
-        tokio::spawn(async move {
-            let server_stream = server.await.unwrap();
-            ConsoleLogger::success(format!(
-                "Connected to game server at {}",
-                server_stream.peer_addr().unwrap()
-            ));
-            server_stream.set_nodelay(true).unwrap();
-            return server_stream;
-        })
-        .await
+
+        let server_stream = server.await.unwrap();
+        ConsoleLogger::success(format!(
+            "Connected to game server at {}",
+            server_stream.peer_addr().unwrap()
+        ));
+        server_stream.set_nodelay(true).unwrap();
+        return Ok(server_stream);
     }
+
     pub async fn forward_buffers(
-        source: Arc<tokio::sync::Mutex<tokio::net::tcp::OwnedReadHalf>>,
-        destination: Arc<tokio::sync::Mutex<tokio::net::tcp::OwnedWriteHalf>>,
+        source_stream: tokio::net::tcp::OwnedReadHalf,
+        destination_stream: tokio::net::tcp::OwnedWriteHalf,
         direction: String,
     ) {
-        let mut buffer = [0; 10000];
-        let mut source_stream = source.lock().await;
-        let mut destination_stream = destination.lock().await;
+        let mut buffer = [0u8; 10000];
+        let mut source_reader = BufReader::new(source_stream);
+
+        let destination_stream_arc = Arc::new(Mutex::new(destination_stream));
+
+        let mut packet_handler = PacketHandler::new(&destination_stream_arc, direction);
         loop {
-            match source_stream.read(&mut buffer).await {
-                Ok(0) => {
-                    break;
+            //buffer.fill(0);
+            let read_length = match source_reader.read(&mut buffer).await {
+                Ok(n) if n != 0 => n,
+                Ok(_) | Err(_) => {
+                    continue;
                 }
-                Ok(n) => {
-                    ConsoleLogger::info(format!(
-                        "{}: {}",
-                        direction,
-                        String::from_utf8_lossy(&buffer[..n])
-                    ));
-                    destination_stream.write_all(&buffer[..n]).await.unwrap();
-                    // destination_stream.flush().await.unwrap();
-                }
-                Err(e) => {
-                    eprintln!("Failed to read from source stream: {}", e);
-                    break;
-                }
-            }
+            };
+
+            packet_handler.forward(&buffer[0..read_length]).await;
         }
     }
 }
